10d140d7157e2c68e6713a2f2ab52ee2b36ac2d9
baystaub <staubbm@gmail.com>
2021-08-07 11:37:48 -0600

    changin user

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -7,8 +7,8 @@ const resolvers = {
     // deleted all queries except the me query. added .select instead of .populate.
   Query: {   
     me: async (parent, args, context) => {
-      if (context.user) {
-        const userData = User.findOne({ _id: context.user._id }).select('-__v -password');
+      if (context.User) {
+        const userData = User.findOne({ _id: context.User._id }).select('-__v -password');
         return userData;
       }
       throw new AuthenticationError('You need to be logged in!');
@@ -17,31 +17,31 @@ const resolvers = {
 // kept mutation the same as classwork activity 26 until savebook section
   Mutation: {
     addUser: async (parent, { username, email, password }) => {
-      const user = await User.create({ username, email, password });
-      const token = signToken(user);
-      return { token, user };
+      const User = await User.create({ username, email, password });
+      const token = signToken(User);
+      return { token, User };
     },
     login: async (parent, { email, password }) => {
-      const user = await User.findOne({ email });
+      const User = await User.findOne({ email });
 
-      if (!user) {
-        throw new AuthenticationError('No user found with this email address');
+      if (!User) {
+        throw new AuthenticationError('No User found with this email address');
       }
 
-      const correctPw = await user.isCorrectPassword(password);
+      const correctPw = await User.isCorrectPassword(password);
 
       if (!correctPw) {
         throw new AuthenticationError('Incorrect credentials');
       }
 
-      const token = signToken(user);
+      const token = signToken(User);
 
-      return { token, user };
+      return { token, User };
     },
     saveBook: async (parent, { bookData }, context) => {
-      if (context.user) {
+      if (context.User) {
         const updatedUser = await User.findByIdAndUpdate(
-            { _id: context.user._id },
+            { _id: context.User._id },
             { $push: {savedBooks: { bookData } } },
             { new: true }
         );
@@ -51,12 +51,12 @@ const resolvers = {
       throw new AuthenticationError('You need to be logged in!');
     },
     // addComment: async (parent, { thoughtId, commentText }, context) => {
-    //   if (context.user) {
+    //   if (context.User) {
     //     return Thought.findOneAndUpdate(
     //       { _id: thoughtId },
     //       {
     //         $addToSet: {
-    //           comments: { commentText, commentAuthor: context.user.username },
+    //           comments: { commentText, commentAuthor: context.User.username },
     //         },
     //       },
     //       {
@@ -69,9 +69,9 @@ const resolvers = {
     // },
     // dont need the add comment section
     removeBook: async (parent, { bookId }, context) => {
-      if (context.user) {
+      if (context.User) {
         const updatedUser = await User.findOneAndUpdate(
-          { _id: context.user._id },
+          { _id: context.User._id },
           { $pull: { savedBooks: { bookId} } },
           { new: true }
         );
@@ -81,14 +81,14 @@ const resolvers = {
       throw new AuthenticationError('You need to be logged in!');
     },
     // removeComment: async (parent, { thoughtId, commentId }, context) => {
-    //   if (context.user) {
+    //   if (context.User) {
     //     return Thought.findOneAndUpdate(
     //       { _id: thoughtId },
     //       {
     //         $pull: {
     //           comments: {
     //             _id: commentId,
-    //             commentAuthor: context.user.username,
+    //             commentAuthor: context.User.username,
     //           },
     //         },
     //       },
