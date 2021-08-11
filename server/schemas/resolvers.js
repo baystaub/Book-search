22e16e311d2c6eb49a912ce8e93d6ea785485084
baystaub <staubbm@gmail.com>
2021-08-11 16:59:06 -0600

    trying to please heroku deploying

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,7 +1,7 @@
 // copied a template from class activity 26
 const { AuthenticationError } = require('apollo-server-express');
-const { User } = require('../models/User');
-const { signToken } = require('../utils/auth');
+const { User } = require('../models');
+const { signToken } = require('../tils/auth');
 
 const resolvers = {
     // deleted all queries except the me query. added .select instead of .populate.
@@ -50,23 +50,6 @@ const resolvers = {
       }
       throw new AuthenticationError('You need to be logged in!');
     },
-    // addComment: async (parent, { thoughtId, commentText }, context) => {
-    //   if (context.User) {
-    //     return Thought.findOneAndUpdate(
-    //       { _id: thoughtId },
-    //       {
-    //         $addToSet: {
-    //           comments: { commentText, commentAuthor: context.User.username },
-    //         },
-    //       },
-    //       {
-    //         new: true,
-    //         runValidators: true,
-    //       }
-    //     );
-    //   }
-    //   throw new AuthenticationError('You need to be logged in!');
-    // },
     // dont need the add comment section
     removeBook: async (parent, { bookId }, context) => {
       if (context.User) {
@@ -80,24 +63,6 @@ const resolvers = {
       }
       throw new AuthenticationError('You need to be logged in!');
     },
-    // removeComment: async (parent, { thoughtId, commentId }, context) => {
-    //   if (context.User) {
-    //     return Thought.findOneAndUpdate(
-    //       { _id: thoughtId },
-    //       {
-    //         $pull: {
-    //           comments: {
-    //             _id: commentId,
-    //             commentAuthor: context.User.username,
-    //           },
-    //         },
-    //       },
-    //       { new: true }
-    //     );
-    //   }
-    //   throw new AuthenticationError('You need to be logged in!');
-    // },
-    // didnt need the remove comment
   },
 };
 
